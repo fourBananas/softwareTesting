69e195913f5d8cfec1853d69234774f21d68fa10
fourBananas <seppo.eiranto@live.fi>
2020-11-18 19:35:06 +0200

    hups

--- src/tests/isBuffer.test.js
+++ src/tests/isBuffer.test.js
@@ -3,15 +3,15 @@ import isBuffer from '../isBuffer'
 describe('isBuffer', () => {
 
     test("1. Detects a buffer", () => {
-        expect(isBuffer(new Buffer(2))).toBeTrue()
+        expect(isBuffer(new Buffer(2))).toBeTruthy()
     });
     
-    test("2. Detects not a buffer", () => {
-        expect(isBuffer([1,2])).toBeFalse()
+    test("2. Detects array as not a buffer", () => {
+        expect(isBuffer([1,2])).toBeFalsy()
     });
 
-    test("3. Detects not a buffer", () => {
-        expect(isBuffer(1)).toBeFalse()
+    test("3. Detects integral as not a buffer", () => {
+        expect(isBuffer(1)).toBeFalsy()
     });
 
 })
