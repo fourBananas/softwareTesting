e6b04de92694c73fcf91b559e7e54e0a493cc1e3
Saku Saaranluoma <saku.saaranluoma@tuni.fi>
2020-12-07 18:21:25 +0200

    added describe to divide.test.js file

--- src/tests/divide.test.js
+++ src/tests/divide.test.js
@@ -1,17 +1,20 @@
 import divide from '../divide'
 
-test("1. Divide 20 by 2 to equal 10", () => {
-    expect(divide(20, 2)).toBe(10)
-});
-test("2. Divide -20 by 2 to equal -10", () => {
-    expect(divide(-20, 2)).toBe(-10)
-});
-test("3. Divide 20 by -2 to equal -10", () => {
-    expect(divide(20, -2)).toBe(-10)
-});
-test("4. Divide -20 by -2 to equal 10", () => {
-    expect(divide(-20, -2)).toBe(10)
-});
-test("5. Divide 20 by 2 to equal Nan", () => {
-    expect(divide(20, 0)).toBe(NaN)
-});
+
+describe('Difference', () => {
+    test("1. Divide 20 by 2 to equal 10", () => {
+        expect(divide(20, 2)).toBe(10)
+    });
+    test("2. Divide -20 by 2 to equal -10", () => {
+        expect(divide(-20, 2)).toBe(-10)
+    });
+    test("3. Divide 20 by -2 to equal -10", () => {
+        expect(divide(20, -2)).toBe(-10)
+    });
+    test("4. Divide -20 by -2 to equal 10", () => {
+        expect(divide(-20, -2)).toBe(10)
+    });
+    test("5. Divide 20 by 2 to equal Nan", () => {
+        expect(divide(20, 0)).toBe(NaN)
+    });
+})
