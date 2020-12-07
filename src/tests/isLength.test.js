6dcb1b8a312e3f4102721c2fe7d4c58f25c1c880
fourBananas <seppo.eiranto@live.fi>
2020-12-07 15:46:48 +0200

    Fixed a typo

--- src/tests/isLength.test.js
+++ src/tests/isLength.test.js
@@ -2,31 +2,31 @@ import isLength from '../IsLength'
 
 describe('isLength', () => {
 
-    test("1. Detects a number as a lenght", () => {
+    test("1. Detects a number as a length", () => {
         expect(isLength(1)).toBeTruthy()
     });
     
-    test("2. Detects 0 as a lenght", () => {
+    test("2. Detects 0 as a length", () => {
         expect(isLength(0)).toBeTruthy()
     });
 
-    test("3. Detects '1' as not a lenght", () => {
-        expect(isLenght('1')).toBeFalsy()
+    test("3. Detects '1' as not a length", () => {
+        expect(isLength('1')).toBeFalsy()
     });
 
-    test("4. Detects NaN as not a lenght", () => {
+    test("4. Detects NaN as not a length", () => {
         expect(isLength(NaN)).toBeFalsy()
     });
 
-    test("5. Detects undefined as not a lenght", () => {
+    test("5. Detects undefined as not a length", () => {
         expect(isLength(undefined)).toBeFalsy()
     });
 
-    test("6. Detects null as not a lenght", () => {
+    test("6. Detects null as not a length", () => {
         expect(isLength(null)).toBeFalsy()
     });
 
-    test("7. Detects a negative number as not a lenght", () => {
+    test("7. Detects a negative number as not a length", () => {
         expect(isLength(-1)).toBeFalsy()
     });
 })
