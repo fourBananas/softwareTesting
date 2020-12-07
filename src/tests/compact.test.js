0bb4d9b76d6636ed53908f20ae44b9fb1f6d2982
fourBananas <seppo.eiranto@live.fi>
2020-12-07 17:24:32 +0200

    tweaking tests and added describe

--- src/tests/compact.test.js
+++ src/tests/compact.test.js
@@ -1,17 +1,24 @@
 import compact from '../compact'
 
-test("1. Should return array without falsy values. First value falsy.", () => {
-    expect(compact([0, 1, false, 2, '', 3])).toStrictEqual([1, 2, 3])
-});
+describe('Compact', () => {
 
-test("2. Should return array without falsy values. Second value falsy", () => {
-    expect(compact([1, false, 2, '', 3])).toStrictEqual([1, 2, 3])
-});
+    test("1. Should return array without falsy values. First value falsy.", () => {
+        expect(compact([0, 1, 2, 3])).toStrictEqual([1, 2, 3])
+    });
 
-test("3. Should return array without falsy values. Last value falsy", () => {
-    expect(compact([1, false, 2, '', 3, ''])).toStrictEqual([1, 2, 3])
-});
+    test("2. Should return array without falsy values. Second value falsy", () => {
+        expect(compact([1, false, 2, 3])).toStrictEqual([1, 2, 3])
+    });
 
-test("4. Should return empty array. All values falsy", () => {
-    expect(compact([NaN, false, undefined, '', 0, "", null])).toStrictEqual([])
-});
+    test("3. Should return array without falsy values. Last value falsy", () => {
+        expect(compact([1, 2, 3, ''])).toStrictEqual([1, 2, 3])
+    });
+
+    test("4. Should return array without falsy values. Second value and second to last value are falsy", () => {
+        expect(compact([1, false, 2, '', 3])).toStrictEqual([1, 2, 3])
+    });
+
+    test("5. Should return empty array. All values falsy", () => {
+        expect(compact([NaN, false, undefined, '', 0, "", null])).toStrictEqual([])
+    });
+})
