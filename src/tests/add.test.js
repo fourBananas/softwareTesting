8c40ba6e5fde0344a9174eaa2df361669ba2f40e
fourBananas <seppo.eiranto@live.fi>
2020-12-07 21:16:45 +0200

    style

--- src/tests/add.test.js
+++ src/tests/add.test.js
@@ -1,22 +1,26 @@
 //import { TestScheduler } from 'jest'
 import add from '../add'
 
-test('Adds 2 + 3 to equal 5', () => {
-    expect(add(2,3)).toBe(5)
-});
+describe('Add', () => {
+        
+    test('1. Adds 2 + 3 to equal 5', () => {
+        expect(add(2,3)).toBe(5)
+    });
 
-test('Adds 2 - 3 to equal -1', () => {
-    expect(add(2,-3)).toBe(-1)
-});
+    test('2. Adds 2 - 3 to equal -1', () => {
+        expect(add(2,-3)).toBe(-1)
+    });
 
-test('Adds - 2 - 3 to equal -5', () => {
-    expect(add(-2,-3)).toBe(-5)
-});
+    test('3. Adds - 2 - 3 to equal -5', () => {
+        expect(add(-2,-3)).toBe(-5)
+    });
 
-test('Adds "2" + 3 to equal "23"', () => {
-    expect(add("2",3)).toBe("23")
-});
+    test('4. Adds "2" + 3 to equal "23"', () => {
+        expect(add("2",3)).toBe("23")
+    });
 
-test('Adds "ab" + "c" to equal "abc"', () => {
-    expect(add("ab","c")).toBe("abc")
-});
+    test('5. Adds "ab" + "c" to equal "abc"', () => {
+        expect(add("ab","c")).toBe("abc")
+    });
+
+})
